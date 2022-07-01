4673ef7de205c6a10e4eb021be7d7b6c8fe612de
BIN <senlizishi@users.noreply.github.com>
2022-07-01 18:04:43 +0800

    Add files via upload

---
+++ fromUrlencoded.lua
@@ -0,0 +1,37 @@
+local args, err = ngx.req.get_post_args()
+
+if not args then
+    ngx.say('{"code":-1,"message":"failed to get post args"}');
+    return
+end
+
+function table2json(t)  
+        local function serialize(tbl)  
+                local tmp = {}  
+                for k, v in pairs(tbl) do  
+                        local k_type = type(k)  
+                        local v_type = type(v)  
+                        local key = (k_type == "string" and "\"" .. k .. "\":")  
+                            or (k_type == "number" and "")  
+                        local value = (v_type == "table" and serialize(v))  
+                            or (v_type == "boolean" and tostring(v))  
+                            or (v_type == "string" and "\"" .. v .. "\"")  
+                            or (v_type == "number" and v)  
+                        tmp[#tmp + 1] = key and value and tostring(key) .. tostring(value) or nil  
+                end  
+                if table.maxn(tbl) == 0 then  
+                        return "{" .. table.concat(tmp, ",") .. "}"  
+                else  
+                        return "[" .. table.concat(tmp, ",") .. "]"  
+                end  
+        end  
+        assert(type(t) == "table")  
+        return serialize(t)  
+end  
+
+
+
+ngx.var.request_body_data = table2json(args);
+
+ngx.say('{"code":0,"message":""}');
+
---
+++ postjson.lua
@@ -0,0 +1,4 @@
+ngx.var.request_body_data = ngx.req.get_body_data();
+
+ngx.say('{"code":0,"message":""}');
+
